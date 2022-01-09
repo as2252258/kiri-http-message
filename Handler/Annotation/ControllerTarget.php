56d76698333987726e170effd0c4d0e010d96ce5
Administrator <as2252258@163.com>
2022-01-09 15:33:39 +0800

    1

---
+++ Handler/Annotation/ControllerTarget.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Http\Handler\Annotation;
+
+use Kiri\Annotation\Attribute;
+
+#[\Attribute(\Attribute::TARGET_CLASS)] class ControllerTarget extends Attribute
+{
+}
--- Handler/Note/ControllerTarget.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Http\Handler\Annotation;
-
-use Kiri\Annotation\Attribute;
-
-#[\Attribute(\Attribute::TARGET_CLASS)] class ControllerTarget extends Attribute
-{
-}
