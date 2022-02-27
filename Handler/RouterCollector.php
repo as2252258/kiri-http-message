7a810cdb5094076ba82491753edb3694e408b1a3
向林 <as2252258@163.com>
2022-02-27 16:23:26 +0800

    modify plugin name

--- Handler/RouterCollector.php
+++ Handler/RouterCollector.php
@@ -166,6 +166,7 @@ class RouterCollector implements \ArrayAccess, \IteratorAggregate
 	public function find(string $path, string $method): Handler|int|null
 	{
 		if (!isset($this->_item[$path])) {
+			var_dump($method == 'OPTIONS' ? ($this->_item['*'][$method] ?? 404) : __FUNCTION__);
 			return $method == 'OPTIONS' ? ($this->_item['*'][$method] ?? 404) : 404;
 		}
 		$handler = $this->_item[$path][$method] ?? null;
