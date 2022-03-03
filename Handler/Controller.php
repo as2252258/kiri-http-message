da906885d87ab1b0f5eeb53717dca9f0bf173a5f
向林 <as2252258@163.com>
2022-03-03 18:00:49 +0800

    modify plugin name

--- Handler/Controller.php
+++ Handler/Controller.php
@@ -4,63 +4,30 @@ declare(strict_types=1);
 namespace Kiri\Message\Handler;
 
 
+use Kiri;
 use Kiri\Message\Constrict\RequestInterface;
 use Kiri\Message\Constrict\ResponseInterface;
-use JetBrains\PhpStorm\Pure;
-use Kiri\Di\Container;
-use Kiri;
-use Kiri\Annotation\Inject;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 
 /**
  * Class WebController
  * @package Kiri\Web
- * @property RequestInterface $request
- * @property ResponseInterface $response
- * @property LoggerInterface $logger
- * @property ContainerInterface|Container $container
  */
 class Controller
 {
 
-	/**
-	 * @var RequestInterface
-	 */
-	#[Inject(RequestInterface::class)]
-	public RequestInterface $request;
-
 
 	/**
-	 * @var ResponseInterface
+	 * @param RequestInterface $request
+	 * @param ResponseInterface $response
+	 * @param LoggerInterface $logger
+	 * @param ContainerInterface $container
 	 */
-	#[Inject(ResponseInterface::class)]
-	public ResponseInterface $response;
-
-
-	/**
-	 * @var LoggerInterface
-	 */
-	#[Inject(LoggerInterface::class)]
-	public LoggerInterface $logger;
-
-
-	/**
-	 * @return ContainerInterface
-	 */
-	#[Pure] public function getContainer(): ContainerInterface
+	public function __construct(
+		public RequestInterface $request, public ResponseInterface $response,
+		public LoggerInterface  $logger, public ContainerInterface $container)
 	{
-		return Kiri::getDi();
-	}
-
-
-	/**
-	 * @param $name
-	 * @return mixed
-	 */
-	public function __get($name): mixed
-	{
-		return $this->{'get' . ucfirst($name)}();
 	}
 
 
